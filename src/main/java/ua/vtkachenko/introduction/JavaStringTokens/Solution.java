5beddc428cae1c4d5617bb41dcdf185baaf8cbf1
McLoy <tkachenko.vlad@hotmail.com>
2016-12-02 15:08:04 +0200

    JavaStringTokens

---
+++ src/main/java/ua/vtkachenko/introduction/JavaStringTokens/Solution.java
@@ -0,0 +1,18 @@
+package ua.vtkachenko.introduction.JavaStringTokens;
+
+import java.util.*;
+
+//He is a very very good boy, isn't he?
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        StringTokenizer st= new StringTokenizer(s, "[ !,?._'@]+");
+        System.out.println(st.countTokens());
+        while(st.hasMoreTokens()){
+            System.out.println(st.nextToken());
+        }
+        scan.close();
+    }
+}
