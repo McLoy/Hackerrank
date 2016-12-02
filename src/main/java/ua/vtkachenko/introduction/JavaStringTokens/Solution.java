869bb0e121e52b315970d538af99dd6228fb651b
McLoy <tkachenko.vlad@hotmail.com>
2016-12-02 15:08:32 +0200

    JavaStringTokens

--- src/main/java/ua/vtkachenko/introduction/JavaStringTokens/Solution.java
+++ src/main/java/ua/vtkachenko/introduction/JavaStringTokens/Solution.java
@@ -4,7 +4,6 @@ import java.util.*;
 
 //He is a very very good boy, isn't he?
 public class Solution {
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
