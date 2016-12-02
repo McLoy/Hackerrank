6449a340fa3e4d1170c9646b91f41712757f1eb9
McLoy <tkachenko.vlad@hotmail.com>
2016-12-02 23:18:08 +0200

    String speed test for StringBuilder and StringBuffer

---
+++ src/main/java/ua/vtkachenko/introduction/JavaRegex2DuplicateWords/Solution.java
@@ -0,0 +1,9 @@
+package ua.vtkachenko.introduction.JavaRegex2DuplicateWords;
+
+import java.util.regex.Pattern;
+
+public class Solution {
+    public static void main(String[] args) {
+        Pattern p = Pattern.compile("\\d+");
+    }
+}
