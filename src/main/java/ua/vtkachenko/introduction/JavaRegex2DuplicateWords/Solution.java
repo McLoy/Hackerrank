764b99791f88baf1573ed691e75c1fd0c339fc3a
McLoy <tkachenko.vlad@hotmail.com>
2016-12-03 03:10:42 +0200

    Java Regex 2 - Duplicate Words

--- src/main/java/ua/vtkachenko/introduction/JavaRegex2DuplicateWords/Solution.java
+++ src/main/java/ua/vtkachenko/introduction/JavaRegex2DuplicateWords/Solution.java
@@ -1,9 +1,40 @@
 package ua.vtkachenko.introduction.JavaRegex2DuplicateWords;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+/*
+5
+Goodbye bye bye world world world
+Sam went went to to to his business
+Reya is is the the best player in eye eye game
+in inthe
+Hello hello Ab aB
+ */
 public class Solution {
+
     public static void main(String[] args) {
-        Pattern p = Pattern.compile("\\d+");
+
+        String regex = "\\b(\\w+)(\\b\\W+\\b\\1\\b)*";
+        Pattern p = Pattern.compile(regex, Pattern.MULTILINE+Pattern.CASE_INSENSITIVE);
+
+        Scanner in = new Scanner(System.in);
+        int numSentences = Integer.parseInt(in.nextLine());
+
+        while (numSentences-- > 0) {
+            String input = in.nextLine();
+
+            Matcher m = p.matcher(input);
+
+            // Check for subsequences of input that match the compiled pattern
+            while (m.find()) {
+                input = input.replaceAll(m.group(), m.group(1));
+            }
+
+            // Prints the modified sentence.
+            System.out.println(input);
+        }
+
+        in.close();
     }
 }
